742f45cb1f6ad33c5b1a40fd7a7c174b48623e99
pramod <c10.promod@gmail.com>
2026-05-09 19:48:15 +0545

    Connect with MongoDB database locally

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require("express");
 const cors = require('cors');
+const connectToDb = require('./db/db');
 const app = express();
 
-app.use(cors());
+connectToDb();
 
+app.use(cors());
+// connectToDb();
 
 app.get('/', (req, res)=>{
 res.send('Hello this message from Yatrix Project');
