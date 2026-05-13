7a360ea72712871d799e625202dfae6bbc249c9b
pramod <c10.promod@gmail.com>
2026-05-14 00:41:50 +0545

    Connect MongoDB with Atlas and setup database and colletion

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
 
-function connectToDb(){
-    mongoose.connect(process.env.DB_CONNECT).then(() =>{
-            console.log("Connected to DB");
-        }).catch(err => console.log(err));
+// function connectToDb(){
+//     mongoose.connect(process.env.DB_CONNECT).then(() =>{
+//             console.log("Connected to DB");
+//         }).catch(err => console.log(err));
+// }
+
+const DB_NAME = "yatrix";
+
+
+const connectToDb = async () => {
+    try {
+        const connectionInstace = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("Connected to DB Successfully!");
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstace.connection.host}`);
+        
+    } catch (error) {
+        console.log("MongoDB connection having problem", error);
+        process.exit(1)
+    }
 }
 
 module.exports = connectToDb;
