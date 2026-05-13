1dc03ab40b483a4b8c4cbc83e9e0d9564424cead
pramod <c10.promod@gmail.com>
2026-05-13 19:44:53 +0545

    ready for registration with JWT token

--- Backend/app.js
+++ Backend/app.js
@@ -2,16 +2,20 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require("express");
 const cors = require('cors');
-const connectToDb = require('./db/db');
 const app = express();
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 connectToDb();
 
 app.use(cors());
-// connectToDb();
+connectToDb();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res)=>{
 res.send('Hello this message from Yatrix Project');
-})
+});
+app.use('/users', userRoutes);
 
 module.exports = app;
